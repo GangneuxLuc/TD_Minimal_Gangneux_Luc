e1d12ead3cb858076315fd63f98d336695727ecd
GangneuxLuc <l.gangneux@bdx.ecolescreatives.com>
2025-12-01 18:01:14 +0100

    Fin cours 6/12
    Il me manque le mouvement de la spatule et le système de collision associé

--- My project/Assets/Scripts/Spatule.cs
+++ My project/Assets/Scripts/Spatule.cs
@@ -1,26 +1,44 @@
 using UnityEngine;
 using System.Collections;
+
 public class Spatule : MonoBehaviour
 {
+    [SerializeField] private float swaySpeed;
+    [SerializeField] private float minSway = 0f;
+    [SerializeField] private float maxSway = -0.1f;
+    private float swayAmount;
+
+    Quaternion originalRotation;
     GameObject player;
     PlayerProp playerProp;
-    [SerializeField] Transform pivotPoint;
-    private float AttackSpeed;
+    public Transform pivotPoint;
+
 
     void Start()
     {
         player = GameObject.FindWithTag("Player");
         playerProp = player.GetComponent<PlayerProp>();
-        AttackSpeed = playerProp.AttackSpeed;
-
+        swaySpeed = playerProp.AttackSpeed;
+        originalRotation = transform.rotation;
     }
 
     void Update()
     {
+       StartCoroutine(Sway());
+
+    }
+
+
+    IEnumerator Sway()
+    {
         if (playerProp.isAttacking)
         {
-            Debug.Log("La spatule attaque !");
-            transform.RotateAround(pivotPoint.position, Vector3.up, AttackSpeed * Time.deltaTime);
+            yield return new WaitForSeconds(00.1f);
+            transform.RotateAround(pivotPoint.position, Vector3.up, swayAmount);
+            yield return new WaitForSeconds(swaySpeed / 2);
         }
     }
 }
+    
+
+
