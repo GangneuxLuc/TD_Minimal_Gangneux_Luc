4c41a9132595806140fa2c06b0fa0cffd69a801f
GangneuxLuc <l.gangneux@bdx.ecolescreatives.com>
2025-12-02 23:45:26 +0100

    Rotation de la spatule
    J'ai réussi à faire fonctionner la rotation de ma spatule, maintenant je dois réussir à faire en sorte que la spatule inflige les dégâts.

---
+++ My project/Assets/Scripts/PivotRotation.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+
+public class PivotRotation : MonoBehaviour
+{
+    GameObject player;
+    PlayerProp playerProp;
+
+
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        playerProp = player.GetComponent<PlayerProp>();
+
+    }
+
+    void Update()
+    {
+       
+       StartCoroutine(Rotation());
+
+    }
+    IEnumerator Rotation()
+    {
+        if (playerProp.isAttacking)
+        {
+            transform.Rotate(0f, 0f, -10f);
+            yield return new WaitForSeconds(0.01f);
+            transform.Rotate(0f, 0f, 10f);
+
+        }
+    }
+}
